916b93776656e09ae2d1e6efe67c83827c4e9a95
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2024-06-19 17:16:39 +0200

    metodo add

--- Basket/Program.cs
+++ Basket/Program.cs
@@ -4,27 +4,75 @@
     {
         static void Main(string[] args)
         {
-            var myBasket = new Basket
+            var productList = new List<Product>
             {
                 new Product("Latte", 2.50m),
-                new Product("Pane", 1),
-                new Product("Pizza", 5),
-                new Product("Pasta", 1),
+                new Product("Pane", 1.00m),
+                new Product("Pizza", 5.00m),
+                new Product("Pasta", 1.00m),
                 new Product("Piadine", 2.50m),
-                new Product("Prosciutto", 3),
-                new Product("Salame", 3),
-                new Product("Formaggio", 2),
-                new Product("Patatine", 1),
-                new Product("Acqua", 1)
+                new Product("Prosciutto", 3.00m),
+                new Product("Salame", 3.00m),
+                new Product("Formaggio", 2.00m),
+                new Product("Patatine", 1.00m),
+                new Product("Acqua", 1.00m)
             };
+               
+            var myBasket = new Basket();
+            bool isEnd = false;
 
-            foreach (var item in myBasket)
+            Console.WriteLine("Benvenuto nella tua lista della spesa!\n");
+
+            Console.WriteLine(
+                "Cosa vuoi Fare?\n" +
+                "-'a' Aggiungere un prodotto\n" +
+                "-'r' Rimuovere un prodotto\n" +
+                "-'e' Terminare la lista e vedere il totale"
+                );
+            
+            while (!isEnd)
             {
-                Console.WriteLine($"{item.Name}, {item.Price}");
+                var input = (Console.ReadLine() ?? String.Empty).ToLower().Trim();
+
+                if (!string.IsNullOrEmpty(input) )
+                {
+                    if (input == "a")
+                    {
+                        Console.WriteLine("Aggiungi un prodotto dalla lista utilizzando il suo codice:");
+
+                        for (int i = 0; i < productList.Count; i++)
+                        {
+                            var product = productList[i];   
+                            Console.WriteLine($"[{i}] {product.Name} - {product.Price}");
+                        }
+
+                        var index = productList.Count;
+
+                        while (index >= productList.Count) 
+                        {
+                            var prodotto = Console.ReadLine();
+
+                            if (Int32.TryParse(prodotto, out int numero))
+                            {
+                                index = numero;
+                            }
+                        };
+
+                        myBasket.Add(productList[index]);
+
+                        foreach (var item in myBasket)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+                    else
+                        isEnd = true;
+                }
             }
 
-            myBasket.TotalPrice();
-            Console.WriteLine(myBasket.Total);
+            
+
+
 
 
         }
