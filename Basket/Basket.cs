efea9e507774e22f4a45929f4623b2a96c38a219
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2024-06-19 15:53:16 +0200

    creazione classi

---
+++ Basket/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket
+{
+    public class Basket : List<Product>
+    {
+        public decimal Total { get; set; }
+
+        public Basket() { }
+
+        public void TotalPrice()
+        {
+            Total = this.Sum(x => x.Price);
+        }
+    }
+}
--- Basket/Program.cs
+++ Basket/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            var myBasket = new Basket
+            {
+                new Product("Latte", 2.50m),
+                new Product("Pane", 1),
+                new Product("Pizza", 5),
+                new Product("Pasta", 1),
+                new Product("Piadine", 2.50m),
+                new Product("Prosciutto", 3),
+                new Product("Salame", 3),
+                new Product("Formaggio", 2),
+                new Product("Patatine", 1),
+                new Product("Acqua", 1)
+            };
+
+            foreach (var item in myBasket)
+            {
+                Console.WriteLine($"{item.Name}, {item.Price}");
+            }
+
+            myBasket.TotalPrice();
+            Console.WriteLine(myBasket.Total);
+
+
         }
     }
 }
